f6a6c1a28250bbda1c7b25f8e2bee23e3f198332
mohamedmahmoudhosny <62269128+mohamedmahmoudhosny@users.noreply.github.com>
2023-05-24 20:40:59 +0300

    Add files via upload

---
+++ ETL-Telecom/sql_files/99_ssis_sql_tasks.sql
@@ -0,0 +1,31 @@
+-- used queries in Execute SQL task
+
+-- get the next batch id
+select max(batch_id) + 1 as batch_id from dim_audit
+go
+
+-- Insert into dim audit, to be used inside SSIS / OLEDB Connection
+
+insert into dim_audit ( batch_id, package_name, file_name, rows_extracted, rows_inserted, rows_rejected, SuccessfulProcessingInd)
+output inserted.id as audit_id
+values (
+	8, -- 0 batch id
+	'SSMS Manual Insert', -- 1 package name
+	'SSMS FILE 1', -- 2 file_name
+	NULL, -- 3 rows_extracted
+	NULL, -- 4 rows_inserted
+	NULL, -- 5 rows_rejected
+	'N'
+	)
+go
+
+-- update dim audit, to be used inside SSIS / OLEDB Connection
+
+update dim_audit
+set 
+    [rows_extracted] = ? + ?, -- 0  extracted processed + 1 extracted error
+    [rows_inserted] = ? - ?, -- 2 pre insert -  3 dest error
+    [rows_rejected] = ? + ?, -- 4 extracted error  +  5 dest error
+	SuccessfulProcessingInd = 'Y',
+	updated_at = getdate()
+where id = ? -- 6 audit_id
